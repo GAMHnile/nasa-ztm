79ea6b4b721d41dce5a25c650adcf5b89e2c728a
GAMHnile <georgeamhuweh@gmail.com>
2021-06-13 18:56:01 +0100

    initial push

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const planetsRouter = require('./routes/planets.route');
+const planetsRouter = require("./routes/planets.route");
+const path = require("path");
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}))
-app.use(express.json())
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+app.use(express.static(path.join("..", "public")));
 
 app.use("/planets", planetsRouter);
 
+app.get("/", (req, res) => {
+  return res.sendFile(path.join("..", "public", "index.js"));
+});
 
 module.exports = app;
